a886c5fcd5c2a4ad97930e2951dea48beaf1bb7f
Tunde Samson <odewoletundesamson@gmail.com>
2023-09-23 16:49:49 +0100

    updated commiy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      PROJECT_ID: process.env.PROJECT_ID,
+      ENDPOINT: process.env.ENDPOINT,
+      API_KEY: process.env.API_KEY
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+          config.resolve.fallback = {
+              fs: false
+          }
+      }
+      return config;
+  }
+  }
+  module.exports = nextConfig
